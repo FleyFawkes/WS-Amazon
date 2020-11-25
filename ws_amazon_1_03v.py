60b902d494f8dd59d306cc8368c00469f5de55bd
FleyFawkes <fley1@wp.pl>
2020-11-25 23:28:31 +0100

    new version - search through pages + scrape number

--- ws_amazon_1_03v.py
+++ ws_amazon_1_03v.py
@@ -11,7 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 #     dimensions
 #
 # fix else product_stock exception. -
-# File "./ws_amazon_1_02v.py", line 95, in main
+# File "./ws_amazon_1_03v.py", line 95, in main
 #     stock_product = 'https://www.amazon.com/'+multi_seller_link['href']
 # TypeError: list indices must be integers or slices, not str
 #
@@ -29,54 +29,78 @@ def main():
     options.page_load_strategy = 'normal'
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
+    master_list = []  # list of dictionaries for data
+    item_url_list = []  # list of item urls for data
+    number_on_site = 1  # one of number limit used in number of scrapes. Scrape input is second one
+    page_number = 2  # number of page being scraped
+
     index_2 = f'{index}'
     url = category[f'{category_2[index_2]}']
     driver.get(url)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')  # multi_valued_attributes=None
-    results = soup.find_all('div', {'data-component-type': 's-search-result'})
-    master_list = []
-    item_url_list = []
 
-    for result in results:
-        data_dict = {}
-        try:
-            data_dict['name'] = result.find('span', {'class': 'a-size-base-plus a-color-base a-text-normal'}).text
-        except:
-            data_dict['name'] = result.find('span', {'class': 'a-size-medium a-color-base a-text-normal'}).text
-        try:
-            price = result.find('span', {'class': 'a-offscreen'}).text
-            price = price.replace('$', '')
-            price = float(price)
-            data_dict['price_$'] = price
-        except:
-            data_dict['price_$'] = float(0)
-        try:
-            rating = result.find('span', {'class': 'a-icon-alt'}).text
-            rating = rating.split()
-            rating = float(rating[0])
-            data_dict['rating_out_of_5'] = rating
-        except:
-            data_dict['rating_out_of_5'] = 'None'
-        data_dict['item_url'] = 'https://www.amazon.com/'+result.find('a', {'class': 'a-link-normal a-text-normal'})['href']
-        item_url_list.append(data_dict['item_url'])
-        rews = result.find_all('span', {'class': 'a-size-base'})
-        for rew in rews:
-            if len(rew['class']) == 1:
-                number = rew.text
-                number = number.replace(',', '')
-                try:
-                    number = int(number)
-                    if isinstance(number, int):
-                        data_dict['reviews'] = number
-                except:
-                    continue
-        master_list.append(data_dict)
+    while number_on_site < scrape:
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')  # multi_valued_attributes=None
+        results = soup.find_all('div', {'data-component-type': 's-search-result'})
+        for result in results:
+            data_dict = {}
+            try:
+                data_dict['name'] = result.find('span', {'class': 'a-size-base-plus a-color-base a-text-normal'}).text
+            except:
+                data_dict['name'] = result.find('span', {'class': 'a-size-medium a-color-base a-text-normal'}).text
+            try:
+                price = result.find('span', {'class': 'a-offscreen'}).text
+                price = price.replace('$', '')
+                price = float(price)
+                data_dict['price_$'] = price
+            except:
+                data_dict['price_$'] = float(0)
+            try:
+                rating = result.find('span', {'class': 'a-icon-alt'}).text
+                rating = rating.split()
+                rating = float(rating[0])
+                data_dict['rating_out_of_5'] = rating
+            except:
+                data_dict['rating_out_of_5'] = 'None'
+            data_dict['item_url'] = 'https://www.amazon.com/'+result.find('a', {'class': 'a-link-normal a-text-normal'})['href']
+            item_url_list.append(data_dict['item_url'])
+            rews = result.find_all('span', {'class': 'a-size-base'})
+            for rew in rews:
+                if len(rew['class']) == 1:
+                    number = rew.text
+                    number = number.replace(',', '')
+                    try:
+                        number = int(number)
+                        if isinstance(number, int):
+                            data_dict['reviews'] = number
+                    except:
+                        continue
+            master_list.append(data_dict)
+            number_on_site += 1
+        if number_on_site < scrape:
+            url_2 = soup.find('ul', {'class': 'a-pagination'})
+            for url_2_a in url_2:
+                if 'page=' not in driver.current_url:
+                    link_next = url_2.find_all('a')
+                    for a in link_next:
+                        if a.text == '2':
+                            url_2_link = 'https://www.amazon.com/'+a['href']
+                            page_number += 1
+                            driver.get(url_2_link)
+                if f'page={page_number}' in driver.current_url:
+                    link_page = url_2.find('a')['href']
+                    page_number_2 = page_number + 1
+                    link_page_2 = link_page.replace(f'page={page_number}', f'page={page_number_2}')
+                    link_page_3 = link_page_2.replace(f'sr_pg{page_number}', f'sr_pg{page_number_2}')
+                    page_number += 1
+                    driver.get(link_page_3)
+                else:
+                    break
 
-    a = 0  # master_list index for dictionaries to iterate through.
+    a = 0  # master_list index for dictionaries to iterate through
 
-    for url in item_url_list:
-        driver.get(url)
+    for url_3 in item_url_list:
+        driver.get(url_3)
         soup_product = BeautifulSoup(driver.page_source, 'html.parser')
         for product in soup_product:
             try:
@@ -112,8 +136,7 @@ def main():
                 stock_product_form_1 = stock_product.replace('\n', '')
                 master_list[a].setdefault('in_stock', stock_product_form_1)
             product_details = product.find('div', {'id': 'detailBulletsWrapper_feature_div'})
-
-        # add product detail stuff
+            # add product detail stuff
         a += 1
 
     driver.quit()
@@ -153,8 +176,8 @@ if __name__ == '__main__':
         'Tools & Home Improvement': f'https://www.amazon.com/s?k={keyword}&i=tools-intl-ship&ref=nb_sb_noss',
         'Toys & Games': f'https://www.amazon.com/s?k={keyword}&i=toys-and-games-intl-ship&ref=nb_sb_noss',
         'Video Games': f'https://www.amazon.com/s?k={keyword}&i=videogames-intl-ship&ref=nb_sb_noss',}
-    category_2 = {'0': 'All', '1': 'Arts & Crafts', '2': 'Automotive', '3': 'Baby', '4': 'Beauty & Personal Care',
-        '5': 'Books',
+    category_2 = {'0': 'All-only 7 pages of search', '1': 'Arts & Crafts', '2': 'Automotive', '3': 'Baby',
+        '4': 'Beauty & Personal Care', '5': 'Books',
         '6': 'Computers', '7': 'Digital Music', '8': 'Electronics',
         '9': 'Kindle Store', '10': 'Prime Video', '11': 'Womens Fashion', '12': 'Mens Fashion', '13': 'Girls Fashion',
         '14': 'Boys Fashion', '15': 'Deals', '16': 'Health & Household',
@@ -162,7 +185,9 @@ if __name__ == '__main__':
         '21': 'Music, CDs & Vinyl',
         '22': 'Pet Supplies', '23': 'Software', '24': 'Sports & Outdoors',
         '25': 'Tools & Home Improvement', '26': 'Toys & Games', '27': 'Video Games'}
-    print('Select the category of the search, by typing the index of the category in number.\n'
+    print('select the category of the search, by typing the index of the category in number:\n'
           f'{category_2}')
     index = int(input())
+    print('how many entries would you like to get? type in number (2+)(~50 under 2minutes):')
+    scrape = int(input())
     main()
